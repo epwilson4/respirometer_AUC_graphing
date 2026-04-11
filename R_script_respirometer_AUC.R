3fb736d46b2ee0204ac193de09adb5e6a741325b
Emma Wilson <epwilson4@wisc.edu>
2026-04-10 19:32:14 -0500

    Added in color variations to text and to plot bars, adjusted text size, adjusted positions of the axis titles, altered strain names to include specific snps

--- R_script_respirometer_AUC.R
+++ R_script_respirometer_AUC.R
@@ -43,10 +43,19 @@ plot_media <- function(data, media, parent_strain) {
     ungroup()
   
   ggplot(averaged_data, aes(x = Strain, y = avg_normalized_AUC, fill = Strain)) +
-    geom_bar(stat = "identity", width=0.5) +
+    geom_bar(stat = "identity", width=0.3, color = "black", linewidth = 0.3) +
     geom_errorbar(aes(ymin = avg_normalized_AUC - sd_normalized_AUC, 
                      ymax = avg_normalized_AUC + sd_normalized_AUC),
-                 width = 0.2) + 
+                 width = 0.1) +
+    
+    scale_fill_manual(values = c("Y2084" = "#33CC33",
+                                 "Y2060" = "#FF7733",
+                                 "Y2092" = "#FF3399")) +
+    
+    scale_x_discrete(labels =c("Y2084"= "CYR1 PDR3 Parent",
+                               "Y2060" = "cyr1 pdr3 ALE Strain",
+                               "Y2092" = "cyr1 PDR3")) +
+    
     
     labs(
       title = paste(media),
@@ -55,9 +64,25 @@ plot_media <- function(data, media, parent_strain) {
     ) +
     theme_minimal()+
     theme(legend.position = "none",
-          plot.title = element_text(hjust = 0.5),
-          axis.title = element_text(size = 14),
-          axis.text = element_text(size=12))
+          plot.title = element_text(hjust = 0.5,
+                                    face = "bold"),
+          axis.title.x  = element_text(size = 14,
+                                    face = "bold",
+                                    margin = margin(t = 10)),
+          axis.title.y  = element_text(size = 14,
+                                       face = "bold",
+                                       margin = margin(r = 10)),
+          
+          axis.text.y = element_text(size=12,
+                                   face = "bold",
+                                   color = "black"),
+          axis.text.x = element_text(size = 12,
+                                     face = "bold",
+                                     color = c("Y2084" = "#33CC33",
+                                               "Y2060" = "#FF7733",
+                                               "Y2092" = "#FF3399")),
+          panel.grid.major.x = element_blank())
+
 }
 
 plot_ASGH_DMSO <- plot_media(respirometer_data, "2016 ASGH + 1% DMSO", "Y2084")
